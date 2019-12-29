86cf40b4c1b60ec72b461396c1ac17c4b1d88e35
javales <46269614+javales@users.noreply.github.com>
2019-12-29 12:36:51 -0800

    Finished Section 8 which completes the basics

---
+++ autoDemo-RunningProgramsFromTheCommandLine.py
@@ -0,0 +1,6 @@
+# https://www.udemy.com/course/automate/learn/lecture/3465848?start=0#overview
+## Section 9: Running Programs from the Command Line (Lesson 22)
+
+### 22. Launching Python Programs from Outside IDLE
+###################################################
+
--- autoDemo-moreAboutStrings.py
+++ autoDemo-moreAboutStrings.py
@@ -92,3 +92,81 @@ print("")
 if answer.lower() == 'yes':
     print("You entered some form \"YeS\"")
     print("")
+
+#Check if all characters lowercase or uppercase
+print(answer.islower())
+print(answer.isupper())
+#    print("islower() confirms all lower case")
+
+print("Print answer in uppercase")
+print(answer.upper())
+
+print("Print answer to appear to be now uppercase")
+print("Using isupper() method")
+print(answer.upper().isupper())
+
+"Print confirmation answer is immutable"
+print("Print answer")
+print(answer)
+
+print("")
+
+#other methods: isalpha, isalnum, isdecimal, isspace, istitle, title, startswith, endswith
+
+#join strings with a specified string in between
+print("Joining three strings with the , string using join()")
+print(','.join(['cats','rats','bats']))
+print('')
+
+#split a string up into multiple strings based on string pattern
+print('My name is Simon.'.split('m'))
+print('')
+
+#use ljust and rjust to add whitespace padding to left or right to meet length specified
+print("Hello right-justified to 10")
+print('Hello'.rjust(10))
+print('')
+print("Hello left-justified to 10")
+print('Hello'.ljust(10))
+print('')
+
+#use ljust and rjust to add specified padding to left or right to meet length specified
+print("Hello right-justified to 10")
+print('Hello'.rjust(10,'*'))
+print('')
+print("Hello left-justified to 10")
+print('Hello'.ljust(10,'*'))
+print('')
+
+#use center to center within length specified and pad with whitespace or specified character
+print('Yellow centered to 20')
+print('Yellow'.center(20))
+print('Yellow'.center(20,'+'))
+print('')
+
+#strip, lstrip, and rstrip remove whitespace from ends of string
+#can also specify what string to remove from ends of string
+print('Using one of the strip methods to strip out letters of spam from ends')
+print('SpammusteatsSpam'.strip('ampS'))
+print('')
+
+#replace one string pattern with another within a string
+print('Using replace method to replace e with 3')
+print('Hello there'.replace('e','3'))
+print('')
+
+#import pyperclip to use its copy() and paste() functions to access the clipboard 15:52
+
+
+### 21. String Formatting
+#########################
+
+#Conversion specifiers
+name = 'Alice'
+place = 'Main Street'
+time = '6pm'
+food = 'turnips'
+
+print('Hello %s, you are invited to a party at %s at %s. Please bring %s.' % (name,place,time,food))
+
+#BASIC MATERIAL COMPLETE!
