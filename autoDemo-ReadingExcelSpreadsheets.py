fc5df7f485055ec342bfc13562992e031e05dba3
javales <46269614+javales@users.noreply.github.com>
2020-01-05 15:33:41 -0800

    Created first Section 14 Lesson 42 Reading Excel Spreadsheets

--- autoDemo-ReadingExcelSpreadsheets.py
+++ autoDemo-ReadingExcelSpreadsheets.py
@@ -4,3 +4,4 @@
 # https://www.udemy.com/course/automate/learn/lecture/3465848?start=0#overview
 ## Section 14: Excel, Word, and PDF Documents (Lesson 42-45) - only Lesson 42 - Reading Excel Spreadsheets
 
+# Install third-party module openpyxl with pip install openpyxl
