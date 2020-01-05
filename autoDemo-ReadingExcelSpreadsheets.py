395df3cee0cfe6726546392bf85427e75c4ae6f5
javales <46269614+javales@users.noreply.github.com>
2020-01-05 15:27:39 -0800

    Created first Section 15 Lesson 42 Reading Excel Spreadsheets

---
+++ autoDemo-ReadingExcelSpreadsheets.py
@@ -0,0 +1,6 @@
+# /usr/bin/env python3
+
+##############################################################################
+# https://www.udemy.com/course/automate/learn/lecture/3465848?start=0#overview
+## Section 14: Excel, Word, and PDF Documents (Lesson 42-45) - only Lesson 42 - Reading Excel Spreadsheets
+
